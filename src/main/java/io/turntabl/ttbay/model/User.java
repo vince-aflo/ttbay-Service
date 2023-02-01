4296e81b49a734e9338de600a28d3242f270bed0
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2023-02-01 10:42:16 +0000

    created a user model

--- src/main/java/io/turntabl/ttbay/model/User.java
+++ src/main/java/io/turntabl/ttbay/model/User.java
@@ -3,65 +3,28 @@ package io.turntabl.ttbay.model;
 import io.turntabl.ttbay.model.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
+
 
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
 @Builder
-public class User implements UserDetails {
+public class User   {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false, unique = true)
     private Long id;
-    private String name;
+    private String username;
+    private String fullName;
     @Column(name = "email", nullable = false, unique = true)
     private String email;
     private String picture;
     private Role role;
-
     private String location;
 
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
+
